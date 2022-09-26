679ba82cf55a61c6b4fc297de68f124854df8d67
Eyad Fareh <lavna.89@gmail.com>
2022-09-26 17:44:42 +0300

    "s

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import './App.scss'
 import commentsData from './data.json'
 import Comments from './components/Comments.jsx'
+import AddComment from './components/AddComment.jsx'
 function App() {
+  // check webp support (a better way)
+  var prevImageType = localStorage.getItem('imageType');
+  const [imageType, setImageType] = useState(prevImageType ? prevImageType :"png");
+  useEffect(() => {
+    if(!prevImageType){
+      (function() {
+        var img = new Image();
+        img.onload = function() {
+          var hasWebp = !!(img.height > 0 && img.width > 0);
+          if(hasWebp){
+            setImageType("webp");
+            localStorage.setItem("imageType","webp");
+          }
+        };
+        img.onerror = function() {
+          setImageType("png");
+          localStorage.setItem("imageType","png");
+        };
+        img.src = 'data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==';
+      })();
+    }
+  }, []);
+  // start the actual app
   const [comments, setComments] = useState(commentsData);
-  // check webp support (a bad way)
-  const [imageType, setImageType] = useState("png");
-  (function() {
-    var img = new Image();
-    img.onload = function() {
-      var hasWebp = !!(img.height > 0 && img.width > 0);
-      if(hasWebp)
-        setImageType("webp");
-      
-    };
-    img.onerror = function() {
-      setImageType("png");
-    };
-    img.src = 'data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==';
-  })();
-
   return (
     <div className="App">
       <Comments comments={comments.comments} currentUser={comments.currentUser} imageType={imageType}></Comments>
+      <AddComment currentUser={comments.currentUser} imageType={imageType}/>
     </div>
   )
 }
---
+++ src/components/AddComment.jsx
@@ -0,0 +1,42 @@
+import {useEffect, useState, useRef} from 'react'
+export default function AddUser(props){
+  const [commentData, setCommentData] = useState(""); 
+  const textareaRef = useRef();
+  const [lastScrollHeight,setLastScrollHeight] = useState(100); 
+  useEffect(() => {
+    setLastScrollHeight(textareaRef.current.scrollHeight);
+  },[]);
+  useEffect(() => {
+    console.log("Hello");
+    var textarea = textareaRef.current
+    var rows = parseInt(textarea.getAttribute("rows"));
+    textarea.setAttribute("rows", "1");
+
+    if (textarea.scrollHeight > lastScrollHeight) {
+        rows++;
+    } else if (rows > 1 && textarea.scrollHeight < lastScrollHeight) {
+        rows--;
+    }
+    setLastScrollHeight(textarea.scrollHeight);
+    textarea.setAttribute("rows", rows);
+  }, [commentData])
+  function formSubmitHandlar(e){
+    e.preventDefault();
+  } 
+  function inputHandlar(e){
+    setCommentData(e.target.value);
+  }
+  return (
+    <form className="add-user" onSubmit={formSubmitHandlar} action="">
+      <img width="40" src={props.currentUser.image[props.imageType]} alt="your profile picture" />
+      <textarea
+        type="text"
+        onChange={inputHandlar} 
+        placeholder="Add a comment..."
+        rows="1"
+        ref={textareaRef}
+      ></textarea>
+      <button type="submit">Send</button>
+    </form>
+  )
+}
